bc5b5652b108122d3f74e54a6b0a440eb1cc6c6a
Cloud-Rush <tstarrs@mail.umw.edu>
2015-04-27 11:51:07 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for
+from riotwatcher import Riotwatcher
+from twitch import *
+
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+
+
+@app.route('/register', methods = ['GET','POST'])
+
+
+@app.route('/customs', methods = ['GET', 'POST'])
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 3000)
+
