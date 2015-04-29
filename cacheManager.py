5381e2f2095639e5915dc79f6c6bcc74acf7a8b4
Emorelleum <jmueller@mail.umw.edu>
2015-04-29 15:40:38 +0000

    Multi-usr proofing Added logic to allow a background thread to periodically update database in persistant storage, proofed the other parts to keep logic consistant Also added some extra getters/setters for different user data

--- cacheManager.py
+++ cacheManager.py
@@ -3,6 +3,9 @@ import requests
 import json
 import time
 import praw
+import time
+import copy
+import threading
 from riotwatcher import RiotWatcher
 from riotwatcher import EUROPE_WEST
 from riotwatcher import EUROPE_NORDIC_EAST
@@ -16,6 +19,7 @@ from riotwatcher import RUSSIA
 from riotwatcher import TURKEY
 from twitch import *
 
+cacheMutex = threading.Lock()
 
 riot = RiotWatcher('24d89b10-e6ee-469a-91bd-f5e2d15c9e31')
 twitch = TwitchTV()
@@ -41,10 +45,27 @@ def saveCacheDef(saveData):
 	saveFile.close()
 
 def saveCache():
-	saveFile = open("cacheDatabase.json","w")
+	try:
+		saveFile = open("cacheDatabase.json","w")
+	except:
+		print "error attempting to save cache"
+		return
+	cacheMutex.acquire()
 	json.dump(cacheData,saveFile)
 	saveFile.close()
+	cacheMutex.release()
 
+
+def saveCache2():
+	try:
+		saveFile = open("cacheDatabase2.json","w")
+	except:
+		print "error attempting to save cache"
+		return
+	cacheMutex.acquire()
+	json.dump(cacheData,saveFile)
+	saveFile.close()
+	cacheMutex.release()
 
 def extractSummoner(sumName):
 	summer = riot.get_summoner(name=sumName)
@@ -104,11 +125,13 @@ def updateChampion(name,info):
 		cacheData["champions"][name]["info"] = info
 		cacheData["champions"][name]["stale"] = False
 	else:
+		cacheMutex.acquire()
 		cacheData["champions"][name] = {}
 		cacheData["champions"][name]["time"] = time.time()
 		cacheData["champions"][name]["info"] = info
 		cacheData["champions"][name]["stale"] = False
-	saveCache()
+		cacheMutex.release()
+	#saveCache()
 
 def updateNews(name,info):
 	if name in cacheData["news"]:
@@ -116,11 +139,13 @@ def updateNews(name,info):
 		cacheData["news"][name]["info"] = info
 		cacheData["news"][name]["stale"] = False
 	else:
+		cacheMutex.acquire()
 		cacheData["news"][name] = {}
 		cacheData["news"][name]["time"] = time.time()
 		cacheData["news"][name]["info"] = info
 		cacheData["news"][name]["stale"] = False
-	saveCache()
+		cacheMutex.release()
+	#saveCache()
 
 def updateStreamer(name,info):
 	if name in cacheData["streamers"]:
@@ -128,11 +153,13 @@ def updateStreamer(name,info):
 		cacheData["streamers"][name]["info"] = info
 		cacheData["streamers"][name]["stale"] = False
 	else:
+		cacheMuetex.acquire()
 		cacheData["streamers"][name] = {}
 		cacheData["streamers"][name]["time"] = time.time()
 		cacheData["streamers"][name]["info"] = info
 		cacheData["streamers"][name]["stale"] = False
-	saveCache()
+		cacheMutex.release()
+	#saveCache()
 	
 def updateSummoner(name,info):
 	if name in cacheData["summoners"]:
@@ -140,35 +167,49 @@ def updateSummoner(name,info):
 		cacheData["summoners"][name]["info"] = info
 		cacheData["summoners"][name]["stale"] = False
 	else:
+		cacheMutex.acquire()
 		cacheData["summoners"][name] = {}
 		cacheData["summoners"][name]["time"] = time.time()
 		cacheData["summoners"][name]["info"] = info
 		cacheData["summoners"][name]["stale"] = False
-	saveCache()
+		cacheMutex.release()
+	#saveCache()
 
 #get basic data
 #returns {} if no ifo exists, or if the data is marked as stale
 def getChampionInfo(name):
 	if name in cacheData["champions"] and cacheData["champions"][name]["stale"] == False:
-		return cacheData["champions"][name]["info"]
+		cacheMutex.acquire()
+		ret = copy.deepcopy(cacheData["champions"][name]["info"])
+		cacheMutex.release()
+		return ret
 	else:
 		return {}
 
 def getSummonerInfo(name):
 	if name in cacheData["summoners"] and cacheData["summoners"][name]["stale"] == False:
-		return cacheData["summoners"][name]["info"]
+		cacheMutex.acquire()
+		ret = copy.deepcopy(cacheData["summoners"][name]["info"])
+		cacheMutex.release()
+		return ret
 	else:
 		return {}
 
 def getNewsInfo(name):
 	if name in cacheData["news"] and cacheData["news"][name]["stale"] == False:
-		return cacheData["news"][name]["info"]
+		cacheMutex.acuire()
+		ret = copy.deepcopy(cacheData["news"][name]["info"])
+		cacheMutex.release()
+		return ret
 	else:
 		return {}
 
 def getStreamerInfo(name):
 	if name in cacheData["streamers"] and cacheData["streamers"][name]["stale"] == False:
-		return cacheData["streamers"][name]["info"]
+		cacheMutex.acquire()
+		ret = copy.deepcopy(cacheData["streamers"][name]["info"])
+		cacheMutex.release()
+		return ret
 	else:
 		return {}
 
@@ -211,6 +252,7 @@ def trimCache():
 			else:
 				 prunableChampionKeys.append(name)
 	#delete the elements marked for deletion
+	cacheMutex.acquire()
 	for pruner in prunableSummonerKeys:
 		del cacheData["summoners"][pruner]
 	for pruner in prunableStreamerKeys:
@@ -219,17 +261,28 @@ def trimCache():
 		del cacheData["news"][pruner]
 	for pruner in prunableChampionKeys:
 		del cacheData["champions"][pruner]
+	cacheMutex.release()
 	saveCache()
+
+def upkeep():
+	while True:
+		time.sleep(300)
+		saveCache()
+		time.sleep(300)
+		saveCache2()
 
 def test():
 	updateStreamer("Emorelleum",{"url":"www.spleen.com","title":"Viktor epic fail"})
 	updateNews("Blah", {"Art 1":"la"})
 	#extractRedditData()
 	extractSummoner('CloudRush')
+	extractSummoner('Taikohn')
 	#extractStreams()
 	saveCache()
 
 #setupDatabase()
+upkeepThread = threading.Thread(target = upkeep)
+upkeepThread.start()
 test()
 #trimCache()
 #don't uncomment this unless you want to reset the database
--- userManager.py
+++ userManager.py
@@ -1,15 +1,39 @@
 import json
+import copy
+import threading
+import time
 from passlib.hash import sha256_crypt
 
 userFile = open("userDatabase.json")
 data = json.load(userFile)
 userFile.close()
-
+userMutex = threading.Lock()
 
 def saveUserData():
-	saveFile=open("userDatabase.json","w")
+	saveFile = None
+	try:
+		saveFile=open("userDatabase.json","w")
+	except:
+		print "error when attempting to save user file"
+		return
+	userMutex.acquire()
 	json.dump(data,saveFile)
 	saveFile.close()
+	#print "saved1"
+	userMutex.release()
+
+def saveUserData2():
+	saveFile = None
+	try:
+		saveFile=open("userDatabase2.json","w")
+	except:
+		print "error when attempting to save user file"
+		return
+	userMutex.acquire()
+	json.dump(data,saveFile)
+	saveFile.close()
+	#print "saved2"
+	userMutex.release()
 
 
 def isValid(candidate):
@@ -18,77 +42,141 @@ def isValid(candidate):
 def checkLogin(username, password):
 	hashy = sha256_crypt.encrypt(password)
 	if username not in data:
-		return "Error: No record of the account1"
+		return "Error: No record of the account"
 	if sha256_crypt.verify(password, data[username]["passcode"]):
 		return "check successful"
-	return "Error: No record of the account2"
+	return "Error: No record of the account"
 
 def registerLogin(username, password):
+	if "@" in username:
+		return "Error: Username cannot contain '@' cahracter"
 	if username in data:
 		return "Error: Username already exists"
 	if not isValid(password):
 		return "Error: Password must be at least 8 characters in length"
 	if password == "password":
 		return "Really? you really want to use password? No, just no. Try again"
+	userMutex.lock()
 	data[username] = {}
 	data[username]["streamers"] = []
 	data[username]["champions"] = []
 	data[username]["summoners"] = []
 	data[username]["news"] = []
+	data[username]["showStreams"] = True
+	data[username]["showSummoners"] = True
+	data[username]["showNews"] = True
 	data[username]["passcode"] = sha256_crypt.encrypt(password)
+	userMutex.release()
 	return "registration successful"
 
 def addSummoner(username, summoner):
 	if summoner not in data[username]["summoners"]:
+		userMutex.acquire()
 		data[username]["summoners"].append(summoner)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
 
 def addStreamer(username, streamer):
 	if streamer not in data[username]["streamers"]:
+		userMutex.acquire()
 		data[username]["streamers"].append(streamer)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
 
 def addChampion(username, champ):
 	if champ not in data[username]["champions"]:
+		userMutex.acquire()
 		data[username]["champions"].append(champ)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
 
 def addNews(username, news):
 	if news not in data[username]["news"]:
+		userMutex.acquire()
 		data[username]["news"].append(news)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
+
+def toggleNews(username,val):
+	data["username"]["showNews"] = val
+
+def toggleStreamers(username,val):
+	data["username"]["showStreams"] = val
+
+def toggleSummoners(username,val):
+	data["username"]["showSummoners"] = val
+
+def getToggleNews(username,val):
+	return data["username"]["showNews"]
+
+def getToggleStreamers(username,val):
+	return data["username"]["showStreams"]
+
+def getToggleSummoners(username,val):
+	return data["username"]["showSummoners"]
 
 def removeSummoner(username, summoner):
 	if summoner in data[username]["summoners"]:
+		userMutex.acquire()
 		data[username]["summoners"].remove(summoner)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
 
 def removeStreamer(username, streamer):
 	if streamer in data[username]["streamers"]:
+		userMutex.acquire()
 		data[username]["streamers"].remove(streamer)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
 
 def removeChampion(username, champ):
 	if champ in data[username]["champions"]:
+		userMutex.acquire()
 		data[username]["champions"].remove(champ)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
 
 def removeNews(username, news):
 	if news in data[username]["news"]:
+		userMutex.acquire()
 		data[username]["news"].remove(news)
-	saveUserData()
+		userMutex.release()
+	#saveUserData()
 
 def getStreamers(username):
-	return data[username]["streamers"]
+	userMutex.acquire()
+	ret = copy.deepcopy(data[username]["streamers"])
+	userMutex.release()
+	return ret
 
 def getNews(username):
-	return data[username]["news"]
+	userMutex.acquire()
+	ret = copy.deepcopy(data[username]["news"])
+	userMutex.release()
+	return ret
 
 def getChampions(username):
-	return data[username]["champions"]
+	userMutex.acquire()
+	ret = copy.deepcopy(data[username]["champions"])
+	userMutex.release()
+	return ret
 
 def getSummoners(username):
-	return data[username]["summoners"]
+	userMutex.acquire()
+	ret = copy.deepcopy(data[username]["summoners"])
+	userMutex.release()
+	return ret
+
+#method for thread to periodically save database
+def upkeep():
+	#print "upkeeping"
+	while True:
+		#print "in loop"
+		time.sleep(300)
+		saveUserData()
+		time.sleep(300)
+		saveUserData2()
+
 
 def test():
 	#checkLogin("spleen", "lala") 
@@ -105,7 +193,11 @@ def test():
 	removeNews("El spleeno","Queue")
 	removeNews("El spleeno","Queue")
 	registerLogin("oola","poinsetta")
+	addNews("El spleeno","Viktor foud to be OP3")
 	print getNews("El spleeno")
+	saveUserData()
 
-#test()
-saveUserData()
+upkeepThread = threading.Thread(target = upkeep)
+upkeepThread.start()
+test()
+#saveUserData()
